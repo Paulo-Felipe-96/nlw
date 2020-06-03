72ddcf584b251f6ac2e98350567b66f775493631
Paulo Martins <paulofelipe0896@gmail.com>
2020-06-03 01:15:57 -0300

    Segundo commit

---
+++ scripts/intro.js
@@ -0,0 +1,72 @@
+// document.write('Hello');
+
+// variáveis, tipos de dados
+
+// let myvar = 'Hi'
+// const myconst = 'He'
+
+// document.write(myconst + myvar)
+
+// string
+
+// const s1 = "Isso é uma uma string"
+// const s2 = 'Outra string'
+// const s3 = `Mais uma string`
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true or false
+// const bTrue = true
+// const bFalse = false
+// document.write(bTrue)
+
+// objeto possuem propriedades e funcionalidades
+// const pessoa = {
+//   altura: 180,
+//   idade: 24,
+//   solteiro: false,
+//   correr(){
+//     document.write("executar uma grande logica de correr")
+//   }
+// }
+
+// pessoa.correr()
+
+// Array
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "MyName"}]
+
+// document.write(colecaoDeBolinhas[1])
+// document.writeln()
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funções - Funcionalidades - Atalhos - Reuso de códigos
+// function sayMyName(anyParam) {
+//   let a = anyParam === '' ? document.write('Vazio') : document.write(anyParam)
+// }
+
+// sayMyName('Paulo ')
+// sayMyName('Felipe ')
+// sayMyName('Magalhães ')
+// sayMyName('Martins')
+// sayMyName(' :) ')
+// sayMyName('')
+
+// condicionais
+
+// const notaFinal = 7
+
+// if(notaFinal < 5) {
+//   //caminho 1
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+// loop repeticoes
+
+// for(i = 0; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
